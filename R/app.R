47ecc13840aa5c91c8de9eea3dac3ec711fb7617
gmp26 <gmp26@cam.ac.uk>
2016-08-01 14:36:03 +0100

    More updates caused by package name change

--- R/app.R
+++ R/app.R
@@ -1,6 +1,6 @@
 ######################
 #' Run a shiny app to plot funnels or sliced funnels from uploaded mortality data
 #' @export
-run_app <- function() {
+demo1 <- function() {
   shiny::shinyApp(ui = ui, server = server)
 }
