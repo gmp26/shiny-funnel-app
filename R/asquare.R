e59f526a06ab051f07aa45b950e86e3e513f03b1
gmp26 <gmp26@cam.ac.uk>
2016-08-02 17:18:50 +0100

    Attempting to add a javascript widget

---
+++ R/asquare.R
@@ -0,0 +1,52 @@
+#' asquare
+#'
+#' Draws a 100px x 100px square in a given colour
+#'
+#' @import htmlwidgets
+#'
+#' @export
+asquare <- function(message, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'asquare',
+    x,
+    width = width,
+    height = height,
+    package = 'rcljsdemo'
+  )
+}
+
+#' Shiny bindings for asquare
+#'
+#' Output and render functions for using asquare within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a asquare
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name asquare-shiny
+#'
+#' @export
+asquareOutput <- function(outputId, width = '120px', height = '120px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'asquare', width, height, package = 'rcljsdemo')
+}
+
+#' @rdname asquare-shiny
+#' @export
+renderAsquare <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, asquareOutput, env, quoted = TRUE)
+}
+
---
+++ inst/htmlwidgets/asquare.js
@@ -0,0 +1,29 @@
+HTMLWidgets.widget({
+
+  name: 'asquare',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+        // TODO: code to render the widget, e.g.
+        // el.innerText = x.message;
+        window.mountComponent(el, "asquare");
+        console.log("called mountComponent");
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
